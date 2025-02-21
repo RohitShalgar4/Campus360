0413209d30c35c899aa9ecc520daea03e7d97cff
RohitShalgar4 <rohishalgar04@gmail.com>
2025-02-22 01:52:57 +0530

    added schema for admin

---
+++ backend/models/adminModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const adminModel = new mongoose.Schema({
+    full_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    mobile_No: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: ["Male", "Female", "Other"],
+        required: true
+    } 
+}, { timestamps: true });
+
+export const Admin = mongoose.model("Admin", adminModel);
