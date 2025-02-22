02eaa649400bfe7457537dbd27248ddf84063287
Udaymore741 <orionlee741@gmail.com>
2025-02-23 03:36:43 +0530

    modified vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
   ],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // Changed to 8080
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
