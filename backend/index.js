20af3c7bf539d7f3595c73d1bb7b129ac471dad0
RohitShalgar4 <rohishalgar04@gmail.com>
2025-02-22 19:33:04 +0530

    added routes

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,8 @@ import studentRoute from "./routes/studentRoute.js";
 import adminRoute from "./routes/adminRoute.js";
 import doctorRoute from "./routes/doctorRoute.js";
 import loginRoute from "./routes/loginRoute.js";
+import facilityRoute from "./routes/facilityRoute.js";
+import bookingRoute from "./routes/bookingRoute.js";
 
 dotenv.config({});
 connectDB();
@@ -21,17 +23,17 @@ const allowedOrigins = ['http://localhost:5173'];
 
 app.use(
     cors({
-        origin: (origin, callback) => {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true); // Allow the request
-            } else {
-                callback(new Error('Not allowed by CORS')); // Block the request
-            }
-        },
-        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
-        credentials: true, // Allow cookies and auth headers
+      origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true); // Allow the request
+        } else {
+          callback(new Error('Not allowed by CORS')); // Block the request
+        }
+      },
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+      credentials: true, // Allow cookies and auth headers
     })
-);
+  );
 
 // Routes
 
@@ -39,6 +41,8 @@ app.use("/api/v1/student", studentRoute);
 app.use("/api/v1/admin", adminRoute);
 app.use("/api/v1/doctor", doctorRoute);
 app.use("/api/v1/auth", loginRoute);
+app.use("/api/v1/facilities", facilityRoute);
+app.use("/api/v1/bookings", bookingRoute);
 
 app.listen(PORT, () => {
   console.log(`Server listen at port ${PORT}`);
