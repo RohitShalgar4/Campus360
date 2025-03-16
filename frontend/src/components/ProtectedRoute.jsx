11046d82817ad20b01dbba35a4f6167e086d8e18
RohitShalgar4 <rohishalgar04@gmail.com>
2025-03-16 11:36:57 +0530

    changes in logout

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useAuthStore } from '../stores/authStore';
 
-function ProtectedRoute({ children, allowedRoles = ['admin', 'student','doctor'] }) {
+function ProtectedRoute({ children, allowedRoles = ['admin', 'student', 'doctor'] }) {
   const { isAuthenticated, role } = useAuthStore();
+  const location = useLocation();
 
   if (!isAuthenticated) {
-    return <Navigate to="/login" />;
+    // Save the attempted URL
+    return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
   if (!allowedRoles.includes(role)) {
-    return <Navigate to="/" />;
+    return <Navigate to="/" replace />;
   }
 
   return children;
 }
 
-export default ProtectedRoute
+export default ProtectedRoute;
