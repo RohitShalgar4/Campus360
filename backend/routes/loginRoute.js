aa1cea387f3564e9a9454f806303f1a115406bf3
RohitShalgar4 <rohishalgar04@gmail.com>
2025-03-16 11:35:28 +0530

    updated route for logout

--- backend/routes/loginRoute.js
+++ backend/routes/loginRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {login, logout} from "../controllers/loginController.js";
-// import isAuthenticated from "../middleware/isAuthenticated.js";
+import {login, logout, validateToken} from "../controllers/loginController.js";
+import authenticateToken from "../middleware/authenticateToken.js";
 
 const router = express.Router();
 
 router.route("/login").post(login);
 router.route("/logout").get(logout);
+router.route("/validate").get(authenticateToken, validateToken);
 
 export default router;
